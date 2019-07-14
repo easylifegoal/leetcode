422aea90d31829cf9ec7f2d893855325b4842f21
easylifegoal <easylifegoal@outlook.com>
2019-07-14 11:19:28 +0800

    101

---
+++ 101-Symmetric-Tree/Solution.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ * Recursive solution
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null)
+            return true;
+        if (t1 == null || t2 == null)
+            return false;
+        return t1.val == t2.val
+            && isMirror(t1.left, t2.right)
+            && isMirror(t1.right, t2.left);
+
+    }
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ * iterative solution with queue
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+
+            if (t1 == null && t2 == null)
+                continue;
+            if (t1 == null || t2 == null)
+                return false;
+            if (t1.val != t2.val)
+                return false;
+            queue.offer(t1.left);
+            queue.offer(t2.right);
+            queue.offer(t1.right);
+            queue.offer(t2.left);
+        }
+
+        return true;
+    }
+}
