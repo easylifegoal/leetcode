8b7e69cc530b5c028795bc7306b32ac463926baf
easylifegoal <easylifegoal@outlook.com>
2019-07-19 13:18:10 +0800

    028

---
+++ 028-Implement-strStr()/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null
+                || haystack.length() < needle.length()) return -1;
+        if (needle.length() == 0) return 0;
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (needle.equals(haystack.substring(i, i + needle.length())))
+                return i;
+        }
+
+        return -1;
+    }
+}
