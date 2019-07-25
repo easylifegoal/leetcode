fdb5e5639fe906ba14d2861291a56c201a5b09a1
easylifegoal <easylifegoal@outlook.com>
2019-07-25 17:43:33 +0800

    modify 268

--- 268-Missing-Number/Solution.java
+++ 268-Missing-Number/Solution.java
@@ -9,3 +9,16 @@ class Solution {
         return sum;
     }
 }
+
+//use bit
+class Solution {
+    public int missingNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= i;
+            res ^= nums[i];
+        }
+
+        return res ^ nums.length;
+    }
+}
