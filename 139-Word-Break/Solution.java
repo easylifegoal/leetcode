26db5f1a7970f8dd1df5eee43a8934491675f01b
easylifegoal <easylifegoal@outlook.com>
2019-07-30 17:04:33 +0800

    139

---
+++ 139-Word-Break/Solution.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] == true && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int[] memo = new int[s.length() + 1];
+        Arrays.fill(memo, -1);
+
+        return helper(s, wordDict, 0, memo);
+    }
+
+    private boolean helper(String s, List<String> wordDict,
+                          int index, int[] memo) {
+        if (index == s.length()) return true;
+
+        for (int i = 0; i < wordDict.size(); i++) {
+            String word = wordDict.get(i);
+            if (s.startsWith(word, index)) {
+                int start = index + word.length();
+                if (memo[start] == -1) {
+                    memo[start] = helper(s, wordDict, start, memo) ? 1 : 0;
+                }
+                if (memo[start] == 1) return true;
+            }
+        }
+
+        return false;
+    }
+}
