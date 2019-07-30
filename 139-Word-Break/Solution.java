694ba3d9ba01f6bf0a8e558cd9438cabb18099e8
easylifegoal <easylifegoal@outlook.com>
2019-07-31 01:27:41 +0800

    modify 139

--- 139-Word-Break/Solution.java
+++ 139-Word-Break/Solution.java
@@ -18,6 +18,31 @@ class Solution {
 
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>();
+        boolean[] dp = new boolean[s.length() + 1];
+        int max = 0;
+        int min = s.length();
+        for (String str : wordDict) {
+            set.add(str);
+            min = Math.min(min, str.length());
+            max = Math.max(max, str.length());
+        }
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = min; j <= max && j <= i; j++) {
+                String word = s.substring(i - j, i);
+                if (set.contains(word)) {
+                    dp[i] |= dp[i - j];
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
         int[] memo = new int[s.length() + 1];
         Arrays.fill(memo, -1);
 
